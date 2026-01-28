a64b906b19a29065fe7991cf73e993e5100a26dc
Komal Singh <komalcaerusit@gmail.com>
2026-01-28 11:48:41 +0530

    clean code

--- src/main/java/com/caerus/identity/security/JwtUtil.java
+++ src/main/java/com/caerus/identity/security/JwtUtil.java
@@ -5,85 +5,75 @@ import com.caerus.identity.enums.Role;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
 import java.security.KeyPair;
 import java.util.*;
 import java.util.stream.Collectors;
-import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
 
-  private final KeyPair keyPair;
-  private final long jwtExpiration;
-  private final String keyId;
+    private final KeyPair keyPair;
+    private final long jwtExpiration;
+    private final String keyId;
 
-  public JwtUtil(RsaKeyConfig rsaKeyConfig, JwtProperties properties, String keyId)
-      throws Exception {
-    this.keyPair = rsaKeyConfig.keyPair();
-    this.jwtExpiration = properties.getAccessTokenExpiration();
-    this.keyId = keyId;
-  }
-
-  public String generateAccessToken(UserRolesDto user) {
-    Map<String, Object> claims = new HashMap<>();
-
-    //        List<String> roleNames = user.getRoles().stream()
-    //                .map(Enum::name)
-    //                .toList();
-    //
-    //        List<String> privileges = user.getRoles().stream()
-    //                .flatMap(role -> role.getPrivileges().stream())
-    //                .map(Enum::name)
-    //                .toList();
-
-    // Convert role strings to Role enums
-    Set<Role> roles =
-        user.roles().stream()
-            .map(Role::valueOf) // assumes strings exactly match enum names
-            .collect(Collectors.toSet());
-
-    // Extract role names for JWT
-    List<String> roleNames = roles.stream().map(Enum::name).toList();
-
-    // Extract privileges from all roles and remove duplicates
-    List<String> privileges =
-        roles.stream()
-            .flatMap(role -> role.getPrivileges().stream())
-            .map(Enum::name)
-            .distinct()
-            .toList();
-
-    claims.put("roles", roleNames);
-    claims.put("privileges", privileges);
-
-    return Jwts.builder()
-        .setClaims(claims)
-        .setSubject(user.email())
-        .setIssuedAt(new Date())
-        .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
-        .signWith(keyPair.getPrivate(), SignatureAlgorithm.RS256)
-        .setHeaderParam("kid", keyId)
-        .compact();
-  }
-
-  public Claims extractAllClaims(String token) {
-    return Jwts.parserBuilder()
-        .setSigningKey(keyPair.getPublic())
-        .build()
-        .parseClaimsJws(token)
-        .getBody();
-  }
-
-  public String extractEmail(String token) {
-    return extractAllClaims(token).getSubject();
-  }
-
-  public boolean validateJwtToken(String token) {
-    try {
-      extractAllClaims(token);
-      return true;
-    } catch (Exception e) {
-      return false;
+    public JwtUtil(RsaKeyConfig rsaKeyConfig, JwtProperties properties, String keyId)
+            throws Exception {
+        this.keyPair = rsaKeyConfig.keyPair();
+        this.jwtExpiration = properties.getAccessTokenExpiration();
+        this.keyId = keyId;
     }
-  }
+
+    public String generateAccessToken(UserRolesDto user) {
+        Map<String, Object> claims = new HashMap<>();
+        Set<Role> roles =
+                user.roles().stream()
+                        .map(Role::valueOf)
+                        .collect(Collectors.toSet());
+
+        // Extract role names for JWT
+        List<String> roleNames = roles.stream().map(Enum::name).toList();
+
+        // Extract privileges from all roles and remove duplicates
+        List<String> privileges =
+                roles.stream()
+                        .flatMap(role -> role.getPrivileges().stream())
+                        .map(Enum::name)
+                        .distinct()
+                        .toList();
+
+        claims.put("roles", roleNames);
+        claims.put("privileges", privileges);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.email())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(keyPair.getPrivate(), SignatureAlgorithm.RS256)
+                .setHeaderParam("kid", keyId)
+                .compact();
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(keyPair.getPublic())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
